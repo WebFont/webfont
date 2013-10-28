4c86c80c1e3ac25eafc6424098a9e130b90af028
xVir <danil.skachkov@gmail.com>
2013-10-29 00:38:27 +0700

    Sublime Text 3 support

--- WebFont.py
+++ WebFont.py
@@ -1,6 +1,22 @@
 #coding:utf-8
 
-import sublime, sublime_plugin, json, urllib2, webbrowser, cStringIO, zipfile, os, threading
+import sublime, sublime_plugin
+import json
+import webbrowser
+import zipfile
+import os
+import threading
+
+def is_st3():
+	return sublime.version()[0] == '3'
+
+if is_st3():
+	import urllib.request
+	import io	
+else:
+	import urllib2
+	import cStringIO
+
 
 class DriveSelector:
 
@@ -30,9 +46,15 @@ class DriveSelector:
 		else:
 			self.callback(self.drive_list[index])
 
+	def _show_quick_panel(self, options, done):
+		if is_st3():
+			sublime.set_timeout(lambda: self.window.show_quick_panel(options, done), 10)
+		else:
+			self.window.show_quick_panel(options, done)
+
 	def select(self):
 		self.drive_list = self._get_drives()
-		self.window.show_quick_panel(self.drive_list, self._on_done)
+		self._show_quick_panel(self.drive_list, self._on_done)
 
 
 class FolderBrowser:
@@ -46,10 +68,17 @@ class FolderBrowser:
 			self.curr_dir = os.path.abspath(os.path.dirname(__file__))
 		else:			
 			self.curr_dir = os.path.abspath(curr_dir)
-		try:			
-			self.curr_dir = unicode(self.curr_dir, 'cp1251')
-		except:
-			self.curr_dir = unicode(self.curr_dir)
+
+		if is_st3():
+			try:			
+				self.curr_dir = str(self.curr_dir, 'cp1251')
+			except:
+				self.curr_dir = str(self.curr_dir)	
+		else:
+			try:			
+				self.curr_dir = unicode(self.curr_dir, 'cp1251')
+			except:
+				self.curr_dir = unicode(self.curr_dir)
 
 		self.folder_list = [] 
 		self.driver_selector = DriveSelector(self.window, self._check_and_select)
@@ -74,6 +103,12 @@ class FolderBrowser:
 				subdirs.append(f)
 		return subdirs
 
+	def _show_quick_panel(self, options, done):
+		if is_st3():
+			sublime.set_timeout(lambda: self.window.show_quick_panel(options, done), 10)
+		else:
+			self.window.show_quick_panel(options, done)
+
 	def _check_dir(self, dir_path):
 		try:
 			os.listdir(dir_path)
@@ -90,7 +125,7 @@ class FolderBrowser:
 				dir_path = os.path.normpath(os.path.join(self.curr_dir, dir_name))
 				os.mkdir(dir_path)
 			except Exception as e:
-				sublime.status_message(u'невозможно создать каsталог: "%s"' % dir_path)					
+				sublime.status_message(u'невозможно создать каталог: "%s"' % dir_path)					
 				self._check_and_select(None)
 			else:
 				self._check_and_select(dir_path)
@@ -101,10 +136,13 @@ class FolderBrowser:
 		self.window.show_input_panel(u'Введите имя каталога:', self.new_dir_name, on_done, None, on_cancel)
 
 	def _show_folder_menu(self):
-		self.window.show_quick_panel(self.folder_list, self._folder_selected)
+		self._show_quick_panel(self.folder_list, self._folder_selected)
 
 	def _sort(self, lst):
-		return sorted(lst, cmp=lambda a,b: cmp(a.lower(), b.lower()))
+		if is_st3():
+			return sorted(lst, key = str.lower)
+		else:
+			return sorted(lst, cmp=lambda a,b: cmp(a.lower(), b.lower()))
 
 	def _folder_selected(self, index):
 		if index < 0:
@@ -149,6 +187,19 @@ class FolderBrowser:
 		self._try_to_set_curr_file_dir()
 		self._select_folder()
 
+class WebfontImportFontCommand(sublime_plugin.TextCommand):
+	
+	def __init__(self, view):
+		super(WebfontImportFontCommand, self).__init__(view)
+		pass
+
+	def run(self, edit, text):
+		print("start WebfontImportFontCommand with args " + text)
+		importText = text
+		pos = self.view.sel()[0].a
+		self.view.insert(edit, pos, importText)
+		pass
+
 
 class WebfontCommand(sublime_plugin.WindowCommand):
 
@@ -157,45 +208,81 @@ class WebfontCommand(sublime_plugin.WindowCommand):
 
 	def __init__(self, window):
 		super(WebfontCommand, self).__init__(window)
+		print("Init webFont")
 		self.folder_browser = FolderBrowser(self.window, self._folder_selected)
 		self.font_data = self._download_font_info()		
-		self.archive_url = None	
+		self.archive_url = None
 
 	def _download_font_info(self):		
+		print("_download_font_info")
 		try:
-			fd = urllib2.urlopen(self.URL, timeout=5)
-			result = json.load(fd)
+			if is_st3():
+				fd = urllib.request.urlopen(self.URL, timeout=5).read().decode("utf-8")
+				result = json.loads(fd)	
+			else:
+				fd = urllib2.urlopen(self.URL, timeout=5)
+				result = json.load(fd)
+			
 		except Exception as e:
-			sublime.status_message(u'не удалось обновить список шрифтов')
+			print("exception " + str(e))
+			if is_st3():
+				sublime.error_message(u'не удалось обновить список шрифтов')	
+			else:
+				sublime.status_message(u'не удалось обновить список шрифтов')
 			result = None
 		return result
 
 	def _download_font_archive(self, url):
 		try:
-			zipped_data = urllib2.urlopen(url, timeout=15).read()
-			result = cStringIO.StringIO(zipped_data)
+			if is_st3():
+				zipped_data = urllib.request.urlopen(url, timeout=15).read()
+				result = io.BytesIO(zipped_data)	
+			else:
+				zipped_data = urllib2.urlopen(url, timeout=15).read()
+				result = cStringIO.StringIO(zipped_data)
+			
 		except Exception as e:
-			sublime.status_message(u'не удалось скачать шрифт')
+			print("exception " + str(e))
+			if is_st3():
+				sublime.error_message(u'не удалось скачать шрифт')	
+			else:
+				sublime.status_message(u'не удалось скачать шрифт')
+			
 			result = None
 		return result
 
 	def _unpack_archive(self, fd, dest_folder):
 		try:			
 			dest_folder = os.path.normpath(dest_folder)
+			print("dest folder" + dest_folder)
 			z = zipfile.ZipFile(fd)
-			for name in z.namelist():				
-				file_path = os.path.join(dest_folder, name)
-				outfile = open(file_path, 'wb+')
-   				outfile.write(z.read(name))
-   				outfile.close()
+
+			for name in z.namelist():	
+				if name.endswith('/'):
+					#this is folder
+					folder_path = os.path.join(dest_folder, name)
+					if not os.path.exists(folder_path):
+						os.makedirs(folder_path)
+				else:
+					file_path = os.path.join(dest_folder, name)
+					print("file_path" + file_path)
+					outfile = open(file_path, 'wb+')
+					outfile.write(z.read(name))
+					outfile.close()
+
 			fd.close()
 			return True
 		except Exception as e:
-			sublime.status_message(u'при распаковке шрифта произошла ошибка')
+			print("exception " + str(e))
+			if is_st3():
+				sublime.error_message(u'при распаковке шрифта произошла ошибка')	
+			else:
+				sublime.status_message(u'при распаковке шрифта произошла ошибка')
+			
 			return False
 
 	def run(self):
-
+		print("command start")
 		name_list = [u'* перейти на webfont.ru', u'* обновить список шрифтов', u'* скачать шрифт']
 		if self.font_data is not None:
 			for i in self.font_data:
@@ -203,11 +290,15 @@ class WebfontCommand(sublime_plugin.WindowCommand):
 		self.window.show_quick_panel(name_list, self._selected)
 
 	def _insert(self, text):
-		view = self.window.active_view()		
-		edit = view.begin_edit()		
-		pos = view.sel()[0].a
-		view.insert(edit, pos, text)		
-		view.end_edit(edit)
+		if is_st3():
+			self.window.active_view().run_command("webfont_import_font", {"text" : text })	
+		else:
+			view = self.window.active_view()		
+			edit = view.begin_edit()		
+			pos = view.sel()[0].a
+			view.insert(edit, pos, text)		
+			view.end_edit(edit)
+		
 
 	def _download_font_archive_and_unpack(self, dest_folder, pack_url):
 		fd = self._download_font_archive(pack_url)
@@ -231,6 +322,12 @@ class WebfontCommand(sublime_plugin.WindowCommand):
 		self.archive_url = self.font_data[index]['pack_url']
 		self.folder_browser.select_folder()
 
+	def _show_quick_panel(self, options, done):
+		if is_st3():
+			sublime.set_timeout(lambda: self.window.show_quick_panel(options, done), 10)
+		else:
+			self.window.show_quick_panel(options, done)
+
 	def _selected(self, index):
 		if index < 0: return
 		if index == 0:
@@ -249,7 +346,7 @@ class WebfontCommand(sublime_plugin.WindowCommand):
 			if self.font_data is not None:
 				for i in self.font_data:
 					name_list.append(i['name'])
-			self.window.show_quick_panel(name_list, self._font_archive_selected)
+			self._show_quick_panel(name_list, self._font_archive_selected)
 
 		else:			
 			text = self.font_data[index-3]['import'] + '\n' + self.font_data[index-3]['comments']

acbaaf822f975bd8e0827264131e21572d0d0f85
alext <softlink@bk.ru>
2022-08-15 20:27:28 +0700

    new api and ux improvements

--- WebFont.py
+++ WebFont.py
@@ -4,25 +4,17 @@ import json
 import sublime
 import sublime_plugin
 import webbrowser
+import urllib.request
 
-
-def is_st3():
-    return sublime.version()[0] == '3'
-
-if is_st3():
-    import urllib.request
-else:
-    import urllib2
 
 loc = {
     'en_US': {
         'cant_refresh_fonts': u'can\'t update fonts list',
-        'cant_download_font': u'can\'t download font',
-        'unpack_error': u'unpacking error',
-        'go_to_fontstorage': u'* go to fontstorage.com',
+        'download_font': u'Download font(ttf/otf)',
+        'import_font': u'Import font',
+        'subsetting': u'Subsetting',
+        'view_on_website': u'View on website',
         'update_fonts_list': u'* update fonts list',
-        'open_on_fontstorage': u'* download font',
-        'please_wait': u'please wait',
         'cant_open_website': u'can\'t open website "%s"',
 
         'download_reminder': u'/* Please do not use this import in production. You could download this font from here %s */'
@@ -52,99 +44,80 @@ class WebfontImportFontCommand(sublime_plugin.TextCommand):
 
 class WebfontCommand(sublime_plugin.WindowCommand):
 
-    URL = 'https://fontstorage.com/api/list.json'
-    SITE_URL = 'https://fontstorage.com/'
+    URL = 'https://fontstorage.com/api/plugins.json'
 
     def __init__(self, window):
         super(WebfontCommand, self).__init__(window)
-        # print("Init Fontstorage plugin")
-        self.font_data = self._download_font_info()
+        self.data = self._download_font_info()
+        self.font_data = self.data['fonts']
+        self.urls = self.data['urls']
+
+    def _site_font_url(self):
+        site_font_url = self.urls['site_url'] + '/font/' + self.selected_font['font_slug'] + '?from=sublime'
+        return site_font_url 
+        
+    def _download_font_url(self):
+        download_font_url = self.urls['download_url'] + self.selected_font['slug'] + self.selected_font['slug'] + '.zip'
+        return download_font_url
+
+    def _import_font_url(self):
+        import_font_url = '@import "' + self.urls['import_url'] + self.selected_font['slug'] + '.css";'
+        return import_font_url
+
+    def _subsetting_font_url(self):
+        subsetting_font_url = self.urls['converter_url'] + '#' + self.selected_font['font_slug']
+        return subsetting_font_url
+
 
     def _download_font_info(self):
-        # print("_download_font_info")
         try:
-            if is_st3():
-                fd = urllib.request.urlopen(
-                    self.URL, timeout=5).read().decode("utf-8")
-                result = json.loads(fd)
-            else:
-                fd = urllib2.urlopen(self.URL, timeout=5)
-                result = json.load(fd)
+            fd = urllib.request.urlopen(
+                self.URL, timeout=5).read().decode("utf-8")
+            result = json.loads(fd)
 
         except Exception as e:
             print("exception " + str(e))
-            if is_st3():
-                sublime.error_message(_('cant_refresh_fonts'))
-            else:
-                sublime.status_message(_('cant_refresh_fonts'))
+            sublime.error_message(_('cant_refresh_fonts'))
             result = None
         return result
 
     def run(self):
         print("command start")
-
-        name_list = [
-            _('go_to_fontstorage'),
-            _('update_fonts_list'),
-            _('open_on_fontstorage')]
+        self._download_font_info()
+        name_list = []
         if self.font_data is not None:
             for i in self.font_data:
                 name_list.append(i['name'])
         self.window.show_quick_panel(name_list, self._selected)
 
     def _insert(self, text):
-        if is_st3():
-            self.window.active_view().run_command(
-                "webfont_import_font", {"text": text})
-        else:
-            view = self.window.active_view()
-            edit = view.begin_edit()
-            pos = view.sel()[0].a
-            view.insert(edit, pos, text)
-            view.end_edit(edit)
+        self.window.active_view().run_command(
+            "webfont_import_font", {"text": text})
 
-    def _go_to_site_selected(self, index):
-        if index < 0:
-            return
-
-        font_url = self.font_data[index]['font_url']
-        if font_url is not None:
-            font_url = font_url + "?utm_source=sublime"
-            print("Go to font page: " + font_url)
-            self._open_in_browser(font_url)
-
-    def _show_quick_panel(self, options, done):
-        if is_st3():
-            sublime.set_timeout(
-                lambda: self.window.show_quick_panel(
-                    options, done), 10)
-        else:
-            self.window.show_quick_panel(options, done)
+    def _selected_option(self, index):
+        if index == 0:
+            text = (_('download_reminder') % self._site_font_url()) + '\n' \
+                + self._import_font_url() + '\n' \
+                + self.selected_font['comments']
+            self._insert(text)
+        elif index == 1:
+            print('download')
+            self._open_in_browser(self._site_font_url())
+        elif index == 2:
+            self._open_in_browser(self._subsetting_font_url())
+        elif index == 3:
+            self._open_in_browser(self._site_font_url())
 
     def _selected(self, index):
         if index < 0:
             return
-        if index == 0:
-            self._open_in_browser(self.SITE_URL)
-        elif index == 1:
-            old_font_data = self.font_data
-            self.font_data = self._download_font_info()
-            if self.font_data is None:
-                self.font_data = old_font_data
-            self.run()
-        elif index == 2:
-            name_list = []
-            if self.font_data is not None:
-                for i in self.font_data:
-                    name_list.append(i['name'])
-            self._show_quick_panel(name_list, self._go_to_site_selected)
-
-        else:
-            font_data = self.font_data[index - 3]
-            text = (_('download_reminder') % font_data['font_url']) + '\n' \
-                + font_data['import'] + '\n' \
-                + font_data['comments']
-            self._insert(text)
+        name_list = [
+            _('import_font'),
+            _('download_font'),
+            _('subsetting'),
+            _('view_on_website')]
+        self.selected_font = self.font_data[index]
+        self.window.show_quick_panel(name_list, self._selected_option)
 
     def _open_in_browser(self, url):
         try:
